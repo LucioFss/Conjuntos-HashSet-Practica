b07e4e046508f66c6a50bfa5b9a116119946d4b8
bruno pereyra <brunogeimingforrial@gmail.com>
2025-06-08 19:05:35 -0300

    mira

---
+++ vendoMicolax2k.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class vendoMicolax2k here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class vendoMicolax2k
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class vendoMicolax2k
+     */
+    public vendoMicolax2k()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
